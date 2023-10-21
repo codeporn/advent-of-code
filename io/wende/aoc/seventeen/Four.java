3d0f54a5c85b85cc03f305ce5251a53deadef5a6
Christoph Wende <christoph.wende@diva-e.com>
2023-10-21 16:53:54 +0200

    Added days one and four;

---
+++ io/wende/aoc/seventeen/Four.java
@@ -0,0 +1,25 @@
+package io.wende.aoc.seventeen;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Four extends Puzzle {
+
+  public static void main(final String... args) {
+    Four four = new Four();
+    four.run();
+  }
+
+  private void run() {
+    List<List<String>> phrases = Arrays.stream(input.split("\n")).map(line -> Arrays.stream(line.split(" ")).collect(Collectors.toList())).toList();
+    int phraseCount = phrases.size();
+
+    for(List<String> phrase : phrases) {
+      if(phrase.size() != phrase.stream().distinct().count()) {
+        phraseCount--;
+      }
+    }
+    this.out("Found {} valid passphrases", phraseCount);
+  }
+}
---
+++ io/wende/aoc/seventeen/One.java
@@ -0,0 +1,24 @@
+package io.wende.aoc.seventeen;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class One extends Puzzle{
+
+  public static void main(final String... args) {
+    One one = new One();
+    one.run();
+  }
+
+  private void run() {
+    Pattern p = Pattern.compile("(\\d)(?=\\1)");
+    Matcher m = p.matcher(input);
+    int i = m.results().map(match -> Integer.valueOf(match.group(1))).reduce(0, Integer::sum);
+
+    if(input.charAt(0) == input.charAt(input.length() - 1)) {
+      i += Integer.parseInt(input.substring(0, 1));
+    }
+
+    this.out("Solution is {}", i);
+  }
+}
