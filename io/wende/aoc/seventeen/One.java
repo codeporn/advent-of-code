d018cf2507a4724a78fc0ae64d785c41ce8fdd78
Christoph Wende <christoph@wende.io>
2023-10-23 23:02:16 +0200

    Added day one part two

--- io/wende/aoc/seventeen/One.java
+++ io/wende/aoc/seventeen/One.java
@@ -1,23 +1,43 @@
 package io.wende.aoc.seventeen;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+public class One extends Puzzle {
 
-public class One extends Puzzle{
-
-  public static void main(final String... args) {
-    new One().run();
-  }
-
-  private void run() {
-    Pattern p = Pattern.compile("(\\d)(?=\\1)");
-    Matcher m = p.matcher(input);
-    int i = m.results().map(match -> Integer.valueOf(match.group(1))).reduce(0, Integer::sum);
-
-    if(input.charAt(0) == input.charAt(input.length() - 1)) {
-      i += Integer.parseInt(input.substring(0, 1));
+    public static void main(final String... args) {
+        new One().run();
     }
 
-    this.out("Solution is {}", i);
-  }
+    private void run() {
+        calculate(1);
+        calculate(input.length() / 2);
+    }
+
+    private void calculate(final int steps) {
+        //input = "1212";
+
+        int[] inputArr = this.createInput();
+
+        int result = 0;
+        for (int i = 0; i < inputArr.length; i++) {
+            if (inputArr[i] == inputArr[getIndexAhead(inputArr.length, i, steps)]) {
+                result += inputArr[i];
+            }
+        }
+        this.out("Solution for {} steps ahead is {}", steps, result);
+    }
+
+    private int[] createInput() {
+        int[] arr = new int[input.length()];
+        Character[] charObjectArray =
+                input.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        for (int i = charObjectArray.length; i-- > 0; ) {
+            arr[i] = Integer.parseInt(String.valueOf(charObjectArray[i]));
+        }
+        return arr;
+    }
+
+    private int getIndexAhead(final int size, final int position, final int steps) {
+        int ret = position + steps;
+
+        return ret >= size ? ret - size : ret;
+    }
 }
