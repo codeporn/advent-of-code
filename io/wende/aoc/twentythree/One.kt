d2a17e42314567403ed3773cd5ef7c78c74ad38c
Morathii <morathii@icloud.com>
2023-12-04 14:34:38 +0100

    Remove redundant init in "Task" and make input non-nullable
    Also, readLines may be very slow for large files

--- io/wende/aoc/twentythree/One.kt
+++ io/wende/aoc/twentythree/One.kt
@@ -5,40 +5,42 @@ import io.wende.aoc.common.Task
 class One : Task() {
 
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
+        @JvmStatic
+        fun main(args: Array<String>) {
             One().run()
         }
     }
 
-    fun run (){
-        var sum = this.input?.map {
+    fun run() {
+        var sum = this.input.map {
             "\\d".toRegex().findAll(it)
                 .map { it.value }
                 .joinToString("")
-        }?.map {
+        }.map {
             "${it.first()}${it.last()}".toInt()
-        }?.sum()
+        }.sum()
 
         println("Sum of all coordinates is $sum")
 
         val literals = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
         val numbers = listOf("o1e", "t2o", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e")
 
-        sum = this.input?.map {
-            "\\d".toRegex().findAll(it.replaceAll(literals, numbers)
+        sum = this.input.map {
+            "\\d".toRegex().findAll(
+                it.replaceAll(literals, numbers)
             ).map { it.value }
-                    .joinToString("")
-        }?.map {
+                .joinToString("")
+        }.map {
             "${it.first()}${it.last()}".toInt()
-        }?.sum()
+        }.sum()
 
         println("Sum of all coordinates incl. literals is $sum")
     }
 
-    inline fun String.replaceAll(oldValues: List<String>, replacements:List<String>): String {
+    inline fun String.replaceAll(oldValues: List<String>, replacements: List<String>): String {
         var res = this
         oldValues.forEachIndexed { index, s ->
-            res = res.replace(s, replacements[index] )
+            res = res.replace(s, replacements[index])
         }
         return res
     }
