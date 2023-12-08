ede9552e12e38f1e6a9d07c31e7762d596d6c5f7
Christoph Wende <christoph.wende@diva-e.com>
2023-12-08 12:40:37 +0100

    Added eight

--- io/wende/aoc/common/Util.kt
+++ io/wende/aoc/common/Util.kt
@@ -3,19 +3,5 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-        .toString(16)
-        .padStart(32, '0')
-
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+fun lcm(a: Long, b: Long): Long = a / gcd(a, b) * b
---
+++ io/wende/aoc/twentythree/Eight.kt
@@ -0,0 +1,47 @@
+package io.wende.aoc.twentythree
+
+import io.wende.aoc.common.Task
+import lcm
+
+class Eight : Task() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Eight().run()
+        }
+        var instr: List<Int> = mutableListOf()
+        var steps: Map<String, List<String>> = mutableMapOf()
+    }
+
+    fun run() {
+        this.prepareInput()
+        println("Steps from AAA to reach ZZZ ${this.navigate("AAA", "ZZZ")}")
+        println("Parallel steps from A ending to Z ending nodes ${this.navigateParallel("A", "Z")}")
+    }
+
+    private fun navigateParallel(start: String, dest: String): Long =
+        steps.filter { it.key.endsWith(start) }.keys.map {
+            navigate(it, "Z")
+        }.reduce(::lcm)
+
+    private fun navigate(start: String, dest: String): Long {
+        var count = 0L
+        var pos: String = start
+        do {
+            val i = instr[(if(count < instr.size) count else count % instr.size).toInt()]
+            pos = steps.get(pos)?.get(i) ?: "..."
+            count++
+        } while(!pos.endsWith(dest))
+        return count
+    }
+
+    private fun prepareInput() {
+        instr = this.input[0].map { if(it == 'L') 0 else 1 }
+        steps = this.input.subList(2, this.input.size).associateBy {
+            it.split(" = ")[0]
+        }.mapValues {
+            it.value.split(" = ")[1].replace("[\\(\\)]".toRegex(), "").split(", ")
+        }
+    }
+}
