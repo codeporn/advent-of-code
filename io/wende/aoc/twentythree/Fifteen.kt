39061f5b87c35fdd6523beb96dac7cf72d53f5fc
Christoph Wende <christoph.wende@diva-e.com>
2023-12-14 13:53:16 +0100

    Half the job is preparation

---
+++ io/wende/aoc/twentythree/Fifteen.kt
@@ -0,0 +1,17 @@
+package io.wende.aoc.twentythree
+
+import io.wende.aoc.common.Task
+
+class Fifteen(test: Boolean) : Task(test) {
+
+    companion object {
+    }
+
+    override fun run() {
+        this.prepareInput()
+    }
+
+    private fun prepareInput() {
+    }
+
+}
