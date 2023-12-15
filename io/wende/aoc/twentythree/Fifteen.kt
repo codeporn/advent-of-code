a1a5adfd8ec544526aeef85416e9b7e67f7ebfcd
Christoph Wende <christoph.wende@diva-e.com>
2023-12-15 08:33:30 +0100

    Day fifteen part 1

--- io/wende/aoc/twentythree/Fifteen.kt
+++ io/wende/aoc/twentythree/Fifteen.kt
@@ -5,13 +5,30 @@ import io.wende.aoc.common.Task
 class Fifteen(test: Boolean) : Task(test) {
 
     companion object {
+        var initSequence: List<String> = listOf()
     }
 
     override fun run() {
         this.prepareInput()
+
+        println("\nSum of hash results is ${initSequence.sumOf {
+            it.hash()
+        }}")
     }
 
     private fun prepareInput() {
+        initSequence = this.input[0].split(",")
     }
 
+    private fun String.hash(): Int {
+        var ret: Int = 0
+        if(this.isNotBlank()) {
+            this.chars().forEach {
+                ret += it
+                ret *= 17
+                ret %= 256
+            }
+        } else { return -1 }
+        return ret
+    }
 }
