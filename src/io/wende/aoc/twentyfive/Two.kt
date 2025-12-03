ab48d87fedbd71c89c8f6dee247dbddcfa2f5907
Christoph Wende <christoph@wende.io>
2025-12-03 14:13:38 +0100

    Day 2

---
+++ src/io/wende/aoc/twentyfive/Two.kt
@@ -0,0 +1,31 @@
+package io.wende.aoc.twentyfive
+
+import io.wende.aoc.common.Task
+
+class Two(test: Boolean) : Task(test) {
+
+    companion object {
+        val repeated = """^(\d+)\1$""".toRegex()
+        val repeatedMulti = """^(\d+)\1+$""".toRegex()
+        var invalidSum = 0L
+        var invalidMultiSum = 0L
+    }
+
+    override fun run() {
+        this.input[0].split(",").map { range ->
+            Pair(range.substring(0, range.indexOf("-")).toLong(), range.substring(range.indexOf("-") + 1).toLong())
+        }.forEach { pair ->
+            println(" ${pair.first} ${pair.second}")
+            for(id in pair.first..pair.second) {
+                if(repeated.matches(id.toString())) {
+                    invalidSum += id
+                }
+                if(repeatedMulti.matches(id.toString())) {
+                    invalidMultiSum += id
+                }
+            }
+        }
+        println("Sum of repeated ids is $invalidSum")
+        println("Sum of repeated multiple ids is $invalidMultiSum")
+    }
+}
