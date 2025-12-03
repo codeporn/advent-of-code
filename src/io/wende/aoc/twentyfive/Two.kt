2b9a53b7648619b78d79a367db9450abcc34bc00
Christoph Wende <christoph@wende.io>
2025-12-03 15:09:30 +0100

    Refactored

--- src/io/wende/aoc/twentyfive/Two.kt
+++ src/io/wende/aoc/twentyfive/Two.kt
@@ -7,24 +7,16 @@ class Two(test: Boolean) : Task(test) {
     companion object {
         val repeated = """^(\d+)\1$""".toRegex()
         val repeatedMulti = """^(\d+)\1+$""".toRegex()
-        var invalidSum = 0L
-        var invalidMultiSum = 0L
     }
 
     override fun run() {
-        this.input[0].split(",").map { range ->
-            Pair(range.substringBefore("-").toLong(), range.substring(range.indexOf("-") + 1).toLong())
-        }.forEach { pair ->
-            for(id in pair.first..pair.second) {
-                if(repeated.matches(id.toString())) {
-                    invalidSum += id
-                }
-                if(repeatedMulti.matches(id.toString())) {
-                    invalidMultiSum += id
-                }
-            }
-        }
-        println("Sum of repeated ids is $invalidSum")
-        println("Sum of repeated multiple ids is $invalidMultiSum")
+
+        println("Sum of repeated ids is ${this.input[0].split(",")
+            .map { range -> Pair(range.substringBefore("-").toLong(), range.substring(range.indexOf("-") + 1).toLong()) }
+            .sumOf { pair -> (pair.first..pair.second).sumOf { id -> if(repeated.matches(id.toString())) { id } else { 0 } } }}")
+
+        println("Sum of repeated multiple ids is ${ this.input[0].split(",")
+                .map { range -> Pair(range.substringBefore("-").toLong(), range.substring(range.indexOf("-") + 1).toLong()) }
+                .sumOf { pair -> (pair.first..pair.second).sumOf { id -> if (repeatedMulti.matches(id.toString())) { id } else { 0 } } } }")
     }
 }
