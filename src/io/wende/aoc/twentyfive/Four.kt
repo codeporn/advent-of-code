8c3dd5377c712a10713332c2c6a96d2960cbbfb7
Christoph Wende <christoph@wende.io>
2025-12-04 15:34:39 +0100

    Prettyfying

--- src/io/wende/aoc/twentyfive/Four.kt
+++ src/io/wende/aoc/twentyfive/Four.kt
@@ -29,8 +29,8 @@ class Four(test: Boolean) : Task(test) {
             for(y in 1..map.size - 2) {
                 for(x in 1.. map[y].size - 2) {
                     if(map[y][x] > 0 && accessible(x,y)) {
-                        removeCycle++
                         map[y][x] = 0
+                        removeCycle++
                     }
                 }
             }
@@ -40,14 +40,13 @@ class Four(test: Boolean) : Task(test) {
     }
 
     fun prepare() {
-        map.add(MutableList(this.input[0].length + 2){ 0 })
-
+        map.add(MutableList(this.input[0].length + 2) { 0 })
         this.input.forEach { line ->
-            val mapLine = mutableListOf<Int>(0,0)
-            mapLine.addAll(1, line.map { if(it == '@') { 1} else {0} }.toMutableList())
+            val mapLine = mutableListOf(0, 0)
+            mapLine.addAll(1, line.map { if(it == '@') 1 else 0 }.toMutableList())
             map.add(mapLine)
         }
-        map.add(MutableList(this.input[0].length + 2){ 0 })
+        map.add(MutableList(this.input[0].length + 2) { 0 })
     }
 
     fun accessible(x: Int, y: Int): Boolean {
@@ -57,7 +56,6 @@ class Four(test: Boolean) : Task(test) {
                 rollsFound += map[j][i]
             }
         }
-
         return rollsFound < 4
     }
 }
