557b225d3382432615ae1c372a229ac1dbc38fcb
Christoph Wende <christoph@wende.io>
2025-12-04 16:01:50 +0100

    MD5 and permutations

--- src/io/wende/aoc/common/Util.kt
+++ src/io/wende/aoc/common/Util.kt
@@ -3,6 +3,7 @@ import io.wende.aoc.common.Task
 import io.wende.aoc.common.Util
 import java.io.File
 import java.io.Serializable
+import java.security.MessageDigest
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.math.absoluteValue
@@ -94,8 +95,6 @@ data class Point(var x: Long, var y: Long) : Serializable {
     }
 }
 
-
-
 fun shoelace(points: List<Point>): Long = (0 until points.size - 1).sumOf {
     (points[it].x * points[(it + 1) ].y) - (points[it].y * points[(it + 1)].x)
 }.absoluteValue / 2
@@ -103,3 +102,29 @@ fun shoelace(points: List<Point>): Long = (0 until points.size - 1).sumOf {
 fun innerGraphArea(points: List<Point>): Long = (shoelace(points) - points.size/2 + 1)
 
 fun outerGraphArea(points: List<Point>): Long = (shoelace(points) - points.size/2 + 1) + points.size
+
+@OptIn(ExperimentalStdlibApi::class)
+fun String.md5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(this.toByteArray())
+    return digest.toHexString()
+}
+
+val <T> List<T>.head: T
+    get() = first()
+
+val <T> List<T>.tail: List<T>
+    get() = drop(1)
+
+// Extension function
+
+fun <T> List<T>.permutations(): List<List<T>> {
+    if (isEmpty()) return emptyList()
+    if (size == 1) return listOf(this)
+
+    return tail.permutations().fold(mutableListOf()) { acc, perm ->
+        (0..perm.size).mapTo(acc) { i ->
+            perm.subList(0, i) + head + perm.subList(i, perm.size)
+        }
+    }
+}
